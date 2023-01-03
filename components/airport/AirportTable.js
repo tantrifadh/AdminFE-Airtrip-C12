02d89e7bec807be5b3cc9a413ca9d44d90cb05ea
NaufalAmr <amiruddin0895@gmail.com>
2023-01-03 08:52:51 +0700

    airport list

--- components/airport/AirportTable.js
+++ components/airport/AirportTable.js
@@ -1,88 +1,60 @@
-import Table from 'rc-table';
-import React, { useState } from 'react';
+import Table from "rc-table";
+import React, { useState } from "react";
 import Pagination from "react-js-pagination";
 
-const AirportTable = () => {
-    const columns = [
-        {
-          title: 'No',
-          dataIndex: 'no',
-          key: 'no',
-          className:"text-white bg-gray-800 p-2 border-r-2 border-b-2",
-        },        {
-          title: 'Country Code',
-          dataIndex: 'country_code',
-          key: 'country_name',
-          className:"text-white bg-gray-800 p-2 border-r-2 border-b-2",
-        },
-        {
-          title: 'Airport Name',
-          dataIndex: 'name_airport',
-          key: 'name_airport',
-          className:"text-white bg-gray-800 p-2 border-r-2 border-b-2",
-        },
-        {
-          title: 'iata',
-          dataIndex: 'iata',
-          key: 'iata',
-          className:"text-white bg-gray-800 p-2 border-r-2 border-b-2",
-        },
-        {
-          title: 'icao',
-          dataIndex: 'icao',
-          key: 'icao',
-          className:"text-white bg-gray-800 p-2 border-r-2 border-b-2",
-        },
-        {
-          title: 'Airport Logo',
-          dataIndex: 'imagelogo',
-          key: 'iamgelogo',
-          className:"text-white bg-gray-800 p-2 border-r-2 border-b-2",
-        },
-        {
-          title: 'Address',
-          dataIndex: 'airport_address',
-          key: 'airport_address',
-          className:"text-white bg-gray-800 p-2 border-r-2 border-b-2",
-        },
-        {
-          title: 'Phone',
-          dataIndex: 'airport_phone',
-          key: 'airport_phone',
-          className:"text-white bg-gray-800 p-2 border-r-2 border-b-2",
-        },
-        {
-          title: 'Website',
-          dataIndex: 'website',
-          key: 'website',
-          className:"text-white bg-gray-800 p-2 border-r-2 border-b-2",
-        },
-        {
-          title: 'Operations',
-          dataIndex: '',
-          key: 'operations',
-          className:"text-white bg-gray-800 p-2 border-b-2",
-          render: () => <><a href="#">View</a> | <a href="#">Edit</a> | <a href="#">Delete</a></>,
-          
-        },
-      ];
-      
-      const data = [
-        { no:'01', country_code:'SBY', name_airport:'Juanda', imagelogo:'-', airport_address:'jl. abcd', airport_phone:'0928123434', website:'juanda.com'}
-      ];
+const AirportTable = ({ data }) => {
+  const columns = [
+    {
+      title: "No",
+      dataIndex: "id",
+      key: "no",
+      className: "text-white bg-gray-800 p-2 border-r-2 border-b-2",
+    },
+    {
+      title: "Airport Name",
+      dataIndex: "name",
+      key: "name",
+      className: "text-white bg-gray-800 p-2 border-r-2 border-b-2",
+    },
+    {
+      title: "IATA",
+      dataIndex: "iata",
+      key: "iata",
+      className: "text-white bg-gray-800 p-2 border-r-2 border-b-2",
+    },
 
-      //Pagination
-      const [activePage, setActivePage] = useState(15)
-      const handlePageChange = (pageNumber)=>{
-        setActivePage(pageNumber)
-      }
+    {
+      title: "Address",
+      dataIndex: "address",
+      key: "address",
+      className: "text-white bg-gray-800 p-2 border-r-2 border-b-2",
+    },
 
-    return (
-        <>
-        <Table columns={columns} data={data} rowKey="id"  className='p-4 w-full text-center rc-table-custom font-semibold '/>
-        </>
-        
-    );
+    {
+      title: "Operations",
+      dataIndex: "",
+      key: "operations",
+      className: "text-white bg-gray-800 p-2 border-b-2",
+      // render: () => <><a href="#">View</a> | <a href="#">Edit</a> | <a href="#">Delete</a></>,
+    },
+  ];
+
+  //Pagination
+  const [activePage, setActivePage] = useState(15);
+  const handlePageChange = (pageNumber) => {
+    setActivePage(pageNumber);
+  };
+
+  return (
+    <>
+      <Table
+        columns={columns}
+        data={data}
+        rowKey="id"
+        className="p-4 w-full text-center rc-table-custom font-semibold "
+      />
+    </>
+  );
 };
 
 export default AirportTable;
